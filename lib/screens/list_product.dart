88b49826163de1785094a4c5ecd3d1db6afae4fc
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2023-11-22 14:02:28 +0700

    deploy appps

--- lib/screens/list_product.dart
+++ lib/screens/list_product.dart
@@ -14,7 +14,7 @@ class ProductPage extends StatefulWidget {
 class _ProductPageState extends State<ProductPage> {
   Future<List<Product>> fetchProduct() async {
     // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
-    var url = Uri.parse('http://127.0.0.1:8000/json/');
+    var url = Uri.parse('https://muhkemal-lathif-tutorial.pbp.cs.ui.ac.id/json/');
     var response = await http.get(
       url,
       headers: {"Content-Type": "application/json"},
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -69,8 +69,8 @@ class _LoginPageState extends State<LoginPage> {
                 // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
                 // Untuk menyambungkan Android emulator dengan Django pada localhost,
                 // gunakan URL http://10.0.2.2/
-                final response =
-                    await request.login("http://127.0.0.1:8000/auth/login/", {
+                final response = await request.login(
+                    "https://muhkemal-lathif-tutorial.pbp.cs.ui.ac.id/login/", {
                   'username': username,
                   'password': password,
                 });
